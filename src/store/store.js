5b2a0bcbed58d111227852964562321e37f37130
mahmoudmohamedsh <201727100@std.sci.cu.edu.eg>
2025-05-14 07:40:21 +0300

    add basic configrations of clientDashBoard
    define the client dashboard slice file

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { affiliateReducer } from "./affiliateSlice"
 import { adminReducer } from "./adminSlice"
-
+import { clientDashBoard } from "./clientDashBoardSlice"
 export const store = configureStore({
     reducer: {
         affiliates: affiliateReducer,
-        admins: adminReducer
+        admins: adminReducer,
+        clientDashBoard,
     }
 }) 
