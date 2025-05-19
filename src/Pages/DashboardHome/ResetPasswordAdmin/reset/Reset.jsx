852bce69560b7b13b7551abe90cee9aa33ce937d
ahmedehab72 <a01011618272@gmail.com>
2025-05-19 18:26:01 +0300

    fix confilect

--- src/Pages/DashboardHome/ResetPasswordAdmin/reset/Reset.jsx
+++ src/Pages/DashboardHome/ResetPasswordAdmin/reset/Reset.jsx
@@ -35,13 +35,8 @@ export const Reset = () => {
                 },
             });
 
-<<<<<<< HEAD
             console.log('Response:', response);
- 
             if (response.data.status === true) {
-=======
->>>>>>> 7396e2e4638efdbeb90ce9f6b781eed594822d61
-
                 toast.success('Email sent successfully check your email');
                 navigate('/receive-code-admin');
             }
