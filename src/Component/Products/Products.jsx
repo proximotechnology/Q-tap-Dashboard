bc4901d296725a3e6eb1a2195bc33807fdd90722
mahmoudmohamedsh <201727100@std.sci.cu.edu.eg>
2025-07-16 11:54:37 +0300

    change product card design in register

--- src/Component/Products/Products.jsx
+++ src/Component/Products/Products.jsx
@@ -7,9 +7,70 @@ import DoneIcon from '@mui/icons-material/Done';
 import { toast } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
-import {BASE_URL_IMG,BASE_URL} from '../../utils/constants'
+import { BASE_URL } from '../../utils/constants'
+import { ReactComponent as MenuIcon } from '../../assets/menu.svg';
+ 
+export const ProductCard = ({ handleCardClick, isSelected }) => {
+
+    const { t } = useTranslation()
+    const theme = useTheme();
 
 
+    return <Box onClick={handleCardClick}
+        sx={{
+            position: 'relative',
+            width: "180px", height: "250px",
+            backgroundImage: "url(/images/card.jpg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+            borderRadius: "10px", margin: "30px", padding: "20px",
+            overflow: "hidden"
+        }}>
+        {isSelected && (
+            <DoneIcon
+                sx={{
+                    color: theme.palette.orangePrimary.main,
+                    fontSize: "26px",
+                    position: "relative",
+                    top: "-10px",
+                    right: "-15px",
+                    float: "right",
+                }}
+            />
+        )}
+        <Box>
+
+            <MenuIcon
+                style={{
+                    width: "60px",
+                    height: "60px",
+                    objectFit: "cover",
+                    borderRadius: "10px",
+                    marginBottom: "10px",
+                    color:"#FFF"
+                }}
+            />
+
+
+            <MenuIcon
+                style={{
+                    width: "130px",
+                    height: "130px",
+                    objectFit: "cover",
+                    position: "absolute",
+                    bottom: "-10px",
+                    right: '0',
+                    opacity: '.1',
+                    color:"#FFF",
+                    clipPath: 'inset(0 40% 0 0)',
+                    transform: "translateX(40%)",
+                    
+                }}
+            />
+        </Box>
+        <Typography sx={{ fontSize: "18px", letterSpacing: ".2rem", color: "white" }}>{t("digitalMenu")}</Typography>
+    </Box>
+}
 export const Products = () => {
     const theme = useTheme();
     const navigate = useNavigate();
@@ -87,62 +148,9 @@ export const Products = () => {
                 <Typography sx={{ fontSize: "18px", color: "white" }}>{t("digitalMenu")}</Typography>
                 <span class="icon-waiter" style={{ color: "white", fontSize: "140px", opacity: .1, position: "absolute", top: "58%", left: '38%' }}></span>
             </Box> */}
-            {menus.map((menu) => (
-                <Box onClick={handleCardClick}
-                    key={menu.id}
-                    sx={{
-                        position: 'relative',
-                        width: "180px", height: "250px",
-                        backgroundImage: "url(/images/card.jpg)",
-                        backgroundSize: "cover",
-                        backgroundPosition: "center center",
-                        borderRadius: "10px", margin: "30px", padding: "20px",
-                        overflow: "hidden"
-                    }}>
-                    {isSelected && (
-                        <DoneIcon
-                            sx={{
-                                color: theme.palette.orangePrimary.main,
-                                fontSize: "26px",
-                                position: "relative",
-                                top: "-10px",
-                                right: "-15px",
-                                float: "right",
-                            }}
-                        />
-                    )}
-                    <Box>
-                        {menu.img && (
-                            <img
-                                src={`${BASE_URL_IMG}${menu.img}`}
-                                alt={menu.name}
-                                style={{
-                                    width: "70px",
-                                    height: "70px",
-                                    objectFit: "cover",
-                                    borderRadius: "10px",
-                                    marginBottom: "10px"
-                                }}
-                            />
-                        )}
-                        {menu.img && (
-                            <img
-                                src={`${BASE_URL_IMG}${menu.img}`}
-                                alt={menu.name}
-                                style={{
-                                    width: "100px",
-                                    height: "100px",
-                                    objectFit: "cover",
-                                    position: "absolute",
-                                    bottom: "10px",
-                                    right: "-1%",
-                                    opacity: '.2'
-                                }}
-                            />
-                        )}</Box>
-                    <Typography sx={{ fontSize: "18px", color: "white" }}>{menu.name}</Typography>
-                </Box>
-            ))}
+
+
+            <ProductCard handleCardClick={handleCardClick} isSelected={isSelected} />
 
             <Grid item xs={12}>
                 <Button
